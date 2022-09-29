10efc3c1244d7f4773225a51d6145e073e04ccea
Marco <patrickstar1281@yahoo.com>
2022-09-29 02:26:22 -0500

    saving point did some setup to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Table from './pages/Table'
 import Menu from './pages/Menu';
+import NoMatch from './pages/NoMatch';
+
+const httpLink = createHttpLink({
+  uri: 'graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="">
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Header />
+          <div>
+            <Routes>
+              <Route
+                path="/"
+                element={<Table />}
+              />
+              <Route
+                path="/Menu"
+                element={<Menu />}
+              />
+              <Route
+                path="*"
+                element={<NoMatch />}
+              />
 
-      <Header></Header>
-      <Table></Table>
-      <Menu></Menu>
-      
-      
-    </div>
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
--- client/src/pages/Start.js
+++ client/src/pages/Start.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Start = () => {
   return (
     <div>
-      Uh-oh. we could not find that page...
+
+      <h3>Hello and Welcome! Please sign up or login!</h3>
+
+      <button type="button" class="btn" id="signUp-btn">Sign Up!</button>
+      <button type="button" class="btn" id="login-btn">Login!</button>
+
     </div>
   );
 };
