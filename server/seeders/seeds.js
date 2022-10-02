bff6f141dad4373c262098dd5b489c581852fe58
Marco <patrickstar1281@yahoo.com>
2022-10-02 16:51:34 -0500

    random commit for set up

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -5,18 +5,18 @@ const menuData = require('../seeders/menu.json');
 const restaurantData = require('../seeders/restaurant.json');
 
 db.once('open', async () => {
-  // clean database
+  
   await Menu.deleteMany({});
   await Restaurant.deleteMany({});
  
 
-  // bulk create each model
+  
   const menus = await Menu.insertMany(menuData);
   
   const restaurants = await Restaurant.insertMany(restaurantData);
 
   for (newMenu of menus) {
-    // randomly add each class to a school
+    
     const tempRestaurant = restaurants[Math.floor(Math.random() * restaurants.length)];
     console.log(tempRestaurant);
     console.log(newMenu._id);
