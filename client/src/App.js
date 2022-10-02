6e08d74da94ecc83b5de102e6eb4f15ab9501e61
panecatl <ypanecatl24@gmail.com>
2022-10-01 22:54:13 -0500

    added dark mode. working on resolvers

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Table from './pages/Table'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import ReactSwitch from 'react-switch';
+
+
+import Header from './components/Header';
+import Table from './pages/Table';
 import Menu from './pages/Menu';
 import NoMatch from './pages/NoMatch';
 import TableSetUp from './pages/TableSetUp';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+
+
 
 const httpLink = createHttpLink({
   uri: 'graphql',
@@ -18,16 +24,27 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+export const ThemeContext = createContext("light");
+
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light' ));
+  };
+
   return (
     <ApolloProvider client={client}>
       <Router>
         <Header />
+        <ThemeContext.Provider value= {{ theme, setTheme }}>
+        <div className="App" id={theme}> 
         <Routes>
           <Route
             path="/"
             element={<Table />}
           />
+          
           <Route
             path="/login"
             element={<Login />}
@@ -49,6 +66,9 @@ function App() {
             element={<TableSetUp />}
           />
         </Routes>
+          </div>
+          <ReactSwitch onChange={toggleTheme} checked={theme === 'dark'}/>
+          </ThemeContext.Provider>
       </Router>
     </ApolloProvider>
   );
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,11 @@
 const { Menu, Restaurant } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
 
-const menuText = Menu.obj.menuText
-console.log(menuText)
-
 const resolvers = {
     
     Query: {
         menu: async () => {
-            return menuText.find().sort({ createdAt: -1 });
+            return Menu.find().sort({ createdAt: -1 });
         }
     }
 };
