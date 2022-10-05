d81caa5c4fe2d441dfc541bd6bb2486dfd3c3ca4
Marco <patrickstar1281@yahoo.com>
2022-10-04 20:03:51 -0500

    more heroku setup stuff

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
         app.use(express.static(path.join(__dirname, '../client/build/index.html')));
     }
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
 
